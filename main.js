eeb9c3243ca6c6f975631218fbde2818062bcde6
M. Hassaan Majid <2502086@students.au.edu.pk>
2026-02-26 21:05:39 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+import { add, subtract } from "./math-utils.js";
+
+const sum = add(10, 5);
+const difference = subtract(20, 7);
+
+console.log("10 + 5 =", sum);
+console.log("20 - 7 =", difference);
